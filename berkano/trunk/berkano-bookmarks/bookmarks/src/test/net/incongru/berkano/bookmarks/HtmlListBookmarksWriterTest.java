040be66d4e9096925d4e95c954f92adcc32841c8
gjoseph <gjoseph@5e6c65d6-21f8-0310-a445-fbbf500fbdde>
2005-06-08 22:46:40 +0000

    moving.....

---
+++ berkano/trunk/berkano-bookmarks/bookmarks/src/test/net/incongru/berkano/bookmarks/HtmlListBookmarksWriterTest.java
@@ -0,0 +1,62 @@
+package net.incongru.berkano.bookmarks;
+
+import java.io.StringWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author greg
+ * @author $Author: gj $ (last edit)
+ * @version $Revision: 1.3 $
+ */
+public class HtmlListBookmarksWriterTest extends AbstractBookmarksTestCase {
+    public void testWriter() throws IOException {
+        BookmarksTree tree = setupSimpleTree();
+        HtmlListBookmarksWriter bookmarksWriter = new HtmlListBookmarksWriter();
+        StringWriter sw = new StringWriter();
+        bookmarksWriter.write(tree, sw);
+
+        String expected = "<ul>\n" +
+                          "<li><a href=\"url1\" title=\"name1\">name1</a></li>\n" +
+                          "<li><a href=\"url2\" title=\"name2\">name2</a></li>\n" +
+                          "<li><a href=\"null\" title=\"dir\">dir</a></li>\n" +
+                          "<ul>\n" +
+                          "<li><a href=\"url3\" title=\"name3\">name3</a></li>\n" +
+                          "<li><a href=\"null\" title=\"subdir\">subdir</a></li>\n" +
+                          "<ul>\n" +
+                          "<li><a href=\"url4\" title=\"name4\">name4</a></li>\n" +
+                          "<li><a href=\"url5\" title=\"name5\">name5</a></li>\n" +
+                          "</ul>\n" +
+                          "<li><a href=\"url6\" title=\"name6\">name6</a></li>\n" +
+                          "</ul>\n" +
+                          "<li><a href=\"url7\" title=\"name7\">name7</a></li>\n" +
+                          "</ul>\n";
+        assertEquals(expected, sw.toString());
+    }
+
+    public void testWriterWithOrderChange() throws IOException {
+        BookmarksTree tree = setupSimpleTree();
+        tree.changeOrder(b2, true);
+        tree.changeOrder(dir, false);
+        HtmlListBookmarksWriter bookmarksWriter = new HtmlListBookmarksWriter();
+        StringWriter sw = new StringWriter();
+        bookmarksWriter.write(tree, sw);
+
+        String expected = "<ul>\n" +
+                          "<li><a href=\"url2\" title=\"name2\">name2</a></li>\n" +
+                          "<li><a href=\"url1\" title=\"name1\">name1</a></li>\n" +
+                          "<li><a href=\"url7\" title=\"name7\">name7</a></li>\n" +
+                          "<li><a href=\"null\" title=\"dir\">dir</a></li>\n" +
+                          "<ul>\n" +
+                          "<li><a href=\"url3\" title=\"name3\">name3</a></li>\n" +
+                          "<li><a href=\"null\" title=\"subdir\">subdir</a></li>\n" +
+                          "<ul>\n" +
+                          "<li><a href=\"url4\" title=\"name4\">name4</a></li>\n" +
+                          "<li><a href=\"url5\" title=\"name5\">name5</a></li>\n" +
+                          "</ul>\n" +
+                          "<li><a href=\"url6\" title=\"name6\">name6</a></li>\n" +
+                          "</ul>\n" +
+                          "</ul>\n";
+        assertEquals(expected, sw.toString());
+    }
+}
---
+++ berkano/trunk/berkano-bookmarks/bookmarks/src/test/net/incongru/berkano/tree/TreeTest.java
@@ -0,0 +1,135 @@
+package net.incongru.berkano.tree;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author greg
+ * @author $Author: gj $ (last edit)
+ * @version $Revision: 1.4 $
+ */
+public class TreeTest extends TestCase {
+    public void testAddRemoveObject() {
+        Tree tree = new Tree();
+        tree.addObject("pouet", null);
+        tree.addObject("tralala", "pouet");
+        TreeNode blahNode = tree.addObject("blah", null);
+
+        try {
+            tree.removeObject("bleh");
+            fail();
+        } catch (RuntimeException e) {
+            assertTrue(e.getMessage().startsWith("Can't find parent node for "));
+        }
+        assertEquals(blahNode, tree.removeObject("blah"));
+    }
+
+    public void testAddObjectInParentWithTreeNodeReference() {
+        Tree tree = new Tree();
+        TreeNode pouet = tree.addObject("pouet", null);
+        TreeNode tralala = tree.addObject("tralala", pouet);
+        tree.addObject("blululu", pouet);
+        tree.addObject("halala", tralala);
+
+        TreeNode parent = pouet.findParentOf("halala");
+        assertEquals(tralala, parent);
+    }
+
+    public void testAddObjectInParentWithObjectReference() {
+        Tree tree = new Tree();
+        TreeNode pouet = tree.addObject("pouet", null);
+        tree.addObject("tralala", "pouet");
+        tree.addObject("blululu", "pouet");
+        tree.addObject("halala", "tralala");
+
+        TreeNode parent = pouet.findParentOf("halala");
+        assertNotNull(parent);
+        assertEquals("tralala", parent.getObject());
+    }
+
+    public void testIterator() {
+        Tree tree = new Tree();
+        tree.addObject("pouet", null);
+        tree.addObject("tralala", "pouet");
+        tree.addObject("yopyop", "pouet");
+        tree.addObject("blah", null);
+
+        TreeIterator it = tree.getTreeIterator();
+        try {
+            it.getChildIterator();
+            fail();
+        } catch (IllegalStateException e) {
+        }
+        try {
+            it.hasChildren();
+            fail();
+        } catch (IllegalStateException e) {
+        }
+
+        assertTrue(it.hasNext());
+        assertEquals("pouet", ((TreeNode)it.next()).getObject());
+        assertTrue(it.hasChildren());
+        TreeIterator subIt = it.getChildIterator();
+        assertTrue(subIt.hasNext());
+        assertEquals("tralala", ((TreeNode)subIt.next()).getObject());
+        assertTrue(subIt.hasNext());
+        assertEquals("yopyop", ((TreeNode)subIt.next()).getObject());
+        assertFalse(subIt.hasNext());
+        assertTrue(it.hasNext());
+        assertEquals("blah", ((TreeNode)it.next()).getObject());
+        assertFalse(it.hasChildren());
+        assertFalse(it.hasNext());
+    }
+
+    public void testChangeOrder() {
+        Tree tree = setupFoodTree();
+        tree.changeOrder("emmenthal", true);
+        tree.changeOrder("cheddar", false);
+        try {
+            tree.changeOrder("emmenthal", true);
+            fail();
+        } catch (RuntimeException e) {
+            assertEquals("Node emmenthal can't be moved", e.getMessage());
+        }
+        try {
+            tree.changeOrder("parmiggiano", false);
+            fail();
+        } catch (RuntimeException e) {
+            assertEquals("Node parmiggiano can't be moved", e.getMessage());
+        }
+
+        TreeIterator tit = tree.getTreeIterator();
+        assertTrue(tit.hasNext());
+        assertEquals("cheeses", ((TreeNode)tit.next()).getObject());
+        assertTrue(tit.hasChildren());
+        TreeIterator cheeses = tit.getChildIterator();
+        assertTrue(cheeses.hasNext());
+        assertEquals("emmenthal", ((TreeNode)cheeses.next()).getObject());
+        assertEquals("gouda", ((TreeNode)cheeses.next()).getObject());
+        assertEquals("cheddar", ((TreeNode)cheeses.next()).getObject());
+        assertEquals("parmiggiano", ((TreeNode)cheeses.next()).getObject());
+        assertFalse(cheeses.hasNext());
+
+        assertEquals("fishes", ((TreeNode)tit.next()).getObject());
+        TreeIterator fishes = tit.getChildIterator();
+        assertTrue(fishes.hasNext());
+        assertEquals("salmon", ((TreeNode)fishes.next()).getObject());
+        assertEquals("tuna", ((TreeNode)fishes.next()).getObject());
+        assertEquals("clown fish", ((TreeNode)fishes.next()).getObject());
+        assertFalse(fishes.hasNext());
+    }
+
+    private Tree setupFoodTree() {
+        Tree tree = new Tree();
+        tree.addObject("cheeses", null);
+        tree.addObject("cheddar", "cheeses");
+        tree.addObject("emmenthal", "cheeses");
+        tree.addObject("gouda", "cheeses");
+        tree.addObject("parmiggiano", "cheeses");
+        tree.addObject("fishes", null);
+        tree.addObject("salmon", "fishes");
+        tree.addObject("tuna", "fishes");
+        tree.addObject("clown fish", "fishes");
+        return tree;
+    }
+}
