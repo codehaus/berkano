d11739b9714bc378ea0a8c61cdd3378ab23e31da
gjoseph <gjoseph@5e6c65d6-21f8-0310-a445-fbbf500fbdde>
2005-06-14 00:45:56 +0000

    messing around with repo while merged tools/hibernate, tools/cache, tools/mail into tools/berkano-util

---
+++ berkano/trunk/berkano-tools/berkano-util/src/test/net/incongru/util/hibernate/HibernateTestCase.java
@@ -0,0 +1,33 @@
+package net.incongru.util.hibernate;
+
+import junit.framework.TestCase;
+import net.sf.hibernate.HibernateException;
+import net.sf.hibernate.SessionFactory;
+import net.sf.hibernate.cfg.Configuration;
+import net.sf.hibernate.tool.hbm2ddl.SchemaExport;
+
+/**
+ *
+ * @author greg
+ * @author $Author: gj $ (last edit)
+ * @version $Revision: 1.3 $
+ */
+public abstract class HibernateTestCase extends TestCase {
+    protected static SessionFactory sessionFactory;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        exportSchema();
+    }
+
+    protected abstract void exportSchema() throws HibernateException;
+
+    public static void exportSchema(String[] files) throws HibernateException {
+        Configuration cfg = new Configuration();
+        for (int i = 0; i < files.length; i++) {
+            cfg.addResource(files[i], TestCase.class.getClassLoader());
+        }
+        new SchemaExport(cfg).create(true, true);
+        sessionFactory = cfg.buildSessionFactory(/*new TestInterceptor()*/);
+    }
+}
