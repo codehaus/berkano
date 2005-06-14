c5e34e3b9ad13bc4bd284bfb9158a819c7357572
gjoseph <gjoseph@5e6c65d6-21f8-0310-a445-fbbf500fbdde>
2005-06-14 00:42:09 +0000

    merged tools/hibernate, tools/cache, tools/mail into tools/berkano-util

---
+++ berkano/trunk/berkano-tools/berkano-util/src/java/net/incongru/util/hibernate/EnumType.java
@@ -0,0 +1,68 @@
+package net.incongru.util.hibernate;
+
+import net.sf.hibernate.HibernateException;
+import net.sf.hibernate.UserType;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+/**
+ * An hibernate user type to store java5 enums.
+ * Stores the enum if the form of "pack.age.EnumClass/constantName"
+ * in a VARCHAR field.
+ *
+ * @author greg
+ * @author $Author: gj $ (last edit)
+ * @version $Revision: 1.2 $
+ */
+public class EnumType implements UserType {
+    private static final int[] SQL_TYPES = new int[]{Types.VARCHAR};
+
+    public int[] sqlTypes() {
+        return SQL_TYPES;
+    }
+
+    public Class returnedClass() {
+        return Enum.class;
+    }
+
+    public boolean equals(Object o, Object o1) throws HibernateException {
+        return o == o1;
+    }
+
+    public Object nullSafeGet(ResultSet rs, String[] names, Object owner) throws HibernateException, SQLException {
+        String value = rs.getString(names[0]);
+        if (value != null) {
+            int slashIdx = value.indexOf('/');
+            if (slashIdx < 1 || slashIdx >= value.length() - 1) {
+                throw new HibernateException("Value [" + value + "] is not valid for EnumType");
+            }
+            String clazz = value.substring(0, slashIdx);
+            String constantName = value.substring(slashIdx + 1);
+            try {
+                return Enum.valueOf(Class.forName(clazz), constantName);
+            } catch (ClassNotFoundException e) {
+                throw new HibernateException(clazz + " can not be found: " + e.getMessage(), e);
+            }
+        }
+        return null;
+    }
+
+    public void nullSafeSet(PreparedStatement st, Object value, int index) throws HibernateException, SQLException {
+        String clazz = value.getClass().getName();
+        Enum enumValue = (Enum) value;
+        String constantName = enumValue.name();
+        String output = clazz + '/' + constantName;
+        st.setString(index, output);
+    }
+
+    public Object deepCopy(Object o) throws HibernateException {
+        return o;
+    }
+
+    public boolean isMutable() {
+        return false;
+    }
+}
