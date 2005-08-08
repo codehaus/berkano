7cfc55f070e767e0f10e20f6501ef8d1f60ff00c
gjoseph <gjoseph@5e6c65d6-21f8-0310-a445-fbbf500fbdde>
2005-08-08 20:27:39 +0000

    BERKANO-17 : ApplicationProvider now returns a default app instead of a NullApplication instance, when passed a null key. Throws an IllegalArgumentException when passed an unknown key.

--- berkano/trunk/berkano-user/berkano-user/src/java/net/incongru/berkano/app/ApplicationsProvider.java
+++ berkano/trunk/berkano-user/berkano-user/src/java/net/incongru/berkano/app/ApplicationsProvider.java
@@ -1,54 +1,50 @@
 package net.incongru.berkano.app;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Collection;
 
 /**
  * A simple wrapper around a map of registered applications.
- * Returns an instance of a null-Application
  *
- * TODO : a simple way to determine a default app (the first prefs screen that should be shown)
+ * The default application will be the first one that was registered,
+ * as soon as PICO-243 is fixed.
  *
  * @author greg
  * @author $Author: gj $ (last edit)
  * @version $Revision: 1.1 $
  */
 public class ApplicationsProvider {
-    private static final Application NULL_APP = new NullApp();
-    private Map appsMap;
+    private final String defaultAppName;
+    private final Map<String, Application> appsMap;
 
     public ApplicationsProvider(Application[] apps) {
-        this.appsMap = new HashMap(apps.length);
-        for (int i = 0; i < apps.length; i++) {
-            Application app = apps[i];
+        if (apps.length < 1) {
+            throw new IllegalStateException("No Application was registered");
+        }
+        this.defaultAppName = apps[0].getName();
+        this.appsMap = new HashMap<String, Application>(apps.length);
+        for (Application app : apps) {
             appsMap.put(app.getName(), app);
         }
     }
 
+    /**
+     * Never returns null: if the appName param was null, returns
+     * the default Application.
+     */
     public Application getApp(String appName) {
-        Application app = (Application) appsMap.get(appName);
+        if (appName == null) {
+            appName = defaultAppName;
+        }
+        Application app = appsMap.get(appName);
         if (app == null) {
-            return NULL_APP;
+            throw new IllegalArgumentException("Unknown Application name");
         }
         return app;
     }
 
     public Collection getAllApps() {
         return appsMap.values();
-    }
-
-    private static final class NullApp implements Application {
-        public String getName() {
-            return null;
-        }
-
-        public Object newPreferencesModel() {
-            return null;
-        }
-
-        public PreferenceInfo[] getPreferencesInfo() {
-            return new PreferenceInfo[0];
-        }
     }
 }
