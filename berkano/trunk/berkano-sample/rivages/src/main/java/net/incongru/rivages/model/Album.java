ab58242d45b94152d480abf9f420f46277b0816e
gjoseph <gjoseph@5e6c65d6-21f8-0310-a445-fbbf500fbdde>
2005-09-01 20:28:51 +0000

    build 20050901-2

---
+++ berkano/trunk/berkano-sample/rivages/src/main/java/net/incongru/rivages/model/Album.java
@@ -0,0 +1,16 @@
+package net.incongru.rivages.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author greg
+ * @author $Author: $ (last edit)
+ * @version $Revision: $ 
+ */
+public class Album {
+    private long id;
+    private String name;
+    private String description;
+    private List<Picture> pictures;
+}
