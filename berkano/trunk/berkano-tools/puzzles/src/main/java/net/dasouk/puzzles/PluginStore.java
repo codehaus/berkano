c77b9af3c9af679dea7e96accf6e3057aa07170c
souk <souk@5e6c65d6-21f8-0310-a445-fbbf500fbdde>
2005-11-17 16:00:04 +0000

    added small documentation to PluginStore - test my first commit on codehaus \o/

--- berkano/trunk/berkano-tools/puzzles/src/main/java/net/dasouk/puzzles/PluginStore.java
+++ berkano/trunk/berkano-tools/puzzles/src/main/java/net/dasouk/puzzles/PluginStore.java
@@ -5,19 +5,39 @@ import java.net.URL;
 import java.util.List;
 
 /**
- * The PluginStore's role, if it was not obvious, it is to store plugins.
+ * The PluginStore's role, if it was not obvious, is to store plugins.
  * Besides that, it must be able to retrieve a Plugin from a URL. Once retrieved
  * from that URL (the url can be remote, local, http, file, whatever) the store provides
  * a another url with wich other components will access the plugin package
  *
  *
- * @todo documentation
+ * @author souk
+ * @version 0.1
+ * @todo do not use URL for local storage.
  */
 public interface PluginStore extends Serializable {
+    /**
+     * Retrieves a plugin from the given URL and store it.
+     * @param pluginUrl the URL to download the plugin
+     * @return the URL of the plugin inside the store
+     * @throws StoreException thrown if anything related to the downloading
+     * or the storing of the plugin goes wrong
+     */
     public URL store(URL pluginUrl) throws StoreException;
 
+    /**
+     * removes a plugin from the store
+     * @param pluginUrl the URL of the plugin inside the store
+     * @throws StoreException if the removal encounters problems
+     */
     public void remove(URL pluginUrl) throws StoreException;
 
+    /**
+     * List the urls of the installed plugins. the returned URLs are the
+     * urls of the plugins inside the store.
+     * @return a list of the URLs of the installed plugins.
+     * @throws StoreException
+     */
     public List<URL> getPluginsInStore() throws StoreException;
 
 }
