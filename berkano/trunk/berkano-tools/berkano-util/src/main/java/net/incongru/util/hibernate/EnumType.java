cb8afc2b77fb112f531081088221333e013ed3f9
gjoseph <gjoseph@5e6c65d6-21f8-0310-a445-fbbf500fbdde>
2005-12-28 16:42:26 +0000

    fixed NPE issue with EnumType

--- berkano/trunk/berkano-tools/berkano-util/src/main/java/net/incongru/util/hibernate/EnumType.java
+++ berkano/trunk/berkano-tools/berkano-util/src/main/java/net/incongru/util/hibernate/EnumType.java
@@ -1,5 +1,6 @@
 package net.incongru.util.hibernate;
 
+import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.usertype.UserType;
 
@@ -39,29 +40,32 @@ public class EnumType implements UserType {
 
     public Object nullSafeGet(ResultSet rs, String[] names, Object owner) throws HibernateException, SQLException {
         String value = rs.getString(names[0]);
-        if (value != null) {
-            int slashIdx = value.indexOf('/');
-            if (slashIdx < 1 || slashIdx >= value.length() - 1) {
-                throw new HibernateException("Value [" + value + "] is not valid for EnumType");
-            }
-            String className = value.substring(0, slashIdx);
-            String constantName = value.substring(slashIdx + 1);
-            try {
-                Class clazz = Class.forName(className);
-                return Enum.valueOf(clazz, constantName);
-            } catch (ClassNotFoundException e) {
-                throw new HibernateException(className + " can not be found: " + e.getMessage(), e);
-            }
+        if (value == null) {
+            return null;
         }
-        return null;
+        int slashIdx = value.indexOf('/');
+        if (slashIdx < 1 || slashIdx >= value.length() - 1) {
+            throw new HibernateException("Value [" + value + "] is not valid for EnumType");
+        }
+        String className = value.substring(0, slashIdx);
+        String constantName = value.substring(slashIdx + 1);
+        try {
+            Class clazz = Class.forName(className);
+            return Enum.valueOf(clazz, constantName);
+        } catch (ClassNotFoundException e) {
+            throw new HibernateException(className + " can not be found: " + e.getMessage(), e);
+        }
     }
 
     public void nullSafeSet(PreparedStatement st, Object value, int index) throws HibernateException, SQLException {
-        String clazz = value.getClass().getName();
-        Enum enumValue = (Enum) value;
-        String constantName = enumValue.name();
-        String output = clazz + '/' + constantName;
-        st.setString(index, output);
+        String res = null;
+        if (value != null) {
+            String clazz = value.getClass().getName();
+            Enum enumValue = (Enum) value;
+            String constantName = enumValue.name();
+            res = clazz + '/' + constantName;
+        }
+        Hibernate.STRING.nullSafeSet(st, res, index);
     }
 
     public Object deepCopy(Object o) throws HibernateException {
