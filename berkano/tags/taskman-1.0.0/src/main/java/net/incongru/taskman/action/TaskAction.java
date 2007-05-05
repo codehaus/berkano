5524b7155d0c82f8585eca518318a63c6f6a4180
gjoseph <gjoseph@5e6c65d6-21f8-0310-a445-fbbf500fbdde>
2007-05-05 23:57:43 +0000

    [maven-release-plugin]  copy for tag taskman-1.0.0

---
+++ berkano/tags/taskman-1.0.0/src/main/java/net/incongru/taskman/action/TaskAction.java
@@ -0,0 +1,15 @@
+package net.incongru.taskman.action;
+
+/**
+ *
+ * @author greg
+ * @author $Author: $ (last edit)
+ * @version $Revision: $ 
+ */
+public interface TaskAction {
+    /**
+     * TODO : would pass in a TaskInstance if that wouldn't make it confusing,
+     * because it would mean "execute this task".
+     */
+    void execute(TaskContext taskContext) throws TaskActionExecutionException;
+}
---
+++ berkano/tags/taskman-1.0.0/src/test/java/net/incongru/taskman/AbstractTaskManTestCase.java
@@ -0,0 +1,60 @@
+package net.incongru.taskman;
+
+import net.incongru.taskman.action.TaskAction;
+import net.incongru.taskman.def.TaskDef;
+import net.incongru.taskman.def.TaskDefImpl;
+import net.incongru.taskman.def.TaskDefParser;
+import net.incongru.taskman.testmodel.FirstTaskAction;
+import net.incongru.taskman.testmodel.SecondTaskAction;
+import org.jmock.MockObjectTestCase;
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author greg
+ * @author $Author: $ (last edit)
+ * @version $Revision: $ 
+ */
+public abstract class AbstractTaskManTestCase extends MockObjectTestCase {
+
+    protected TaskInstanceImpl getDummyTaskInstance(TaskDef taskDef) {
+        final TaskInstanceImpl task = new TaskInstanceImpl();
+        task.setDescription("some task instance desc");
+        task.setName("some task instance name");
+        task.setDueDate(new DateTime());
+        task.setId("task instance id");
+        task.setLastReminder(null);
+        task.setTaskDef(taskDef);
+        return task;
+    }
+
+    protected TaskDef getDummyTaskDef() {
+        final TaskDefImpl taskDefImpl = new TaskDefImpl();
+        taskDefImpl.setName("some name");
+        taskDefImpl.setDescription("some desc");
+        taskDefImpl.setDuePeriod(Period.weeks(2));
+        taskDefImpl.setDueDateTimeout(Period.days(3));
+        taskDefImpl.setReminderPeriod(Period.minutes(10));
+        Map<TaskEvent, Class<? extends TaskAction>> map = new HashMap<TaskEvent, Class<? extends TaskAction>>();
+        map.put(TaskEvent.started, FirstTaskAction.class);
+        map.put(TaskEvent.cancelled, SecondTaskAction.class);
+        taskDefImpl.setEventActionMap(map);
+        return taskDefImpl;
+    }
+
+    protected TaskDefParser getDummyTaskDefParser() {
+        final TaskDef taskDef = getDummyTaskDef();
+        assertNull(taskDef.getId());
+        assertNull(taskDef.getVersionId());
+        assertNull(taskDef.getDeploymentDateTime());
+        return new TaskDefParser() {
+            public TaskDef loadTaskDesk() {
+                return taskDef;
+            }
+        };
+    }
+}
