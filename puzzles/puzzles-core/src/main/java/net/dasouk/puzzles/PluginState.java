cb0c5aa8118a6508fdb5ee5d18752609e4673864
gjoseph <gjoseph@5e6c65d6-21f8-0310-a445-fbbf500fbdde>
2007-05-06 14:47:09 +0000

    moving everything up one dir

--- puzzles/puzzles-core/src/main/java/net/dasouk/puzzles/PluginState.java
+++
@@ -1,8 +0,0 @@
-package net.dasouk.puzzles;
-
-/**
- * enumerates the two possible states of a plugin inside a registry: deployed and undeployed.
- */
-public enum PluginState {
-    DEPLOYED, UNDEPLOYED;
-}
