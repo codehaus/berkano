bc2ea1b8c8b1430dacf3029f751314397b6fca6f
gjoseph <gjoseph@5e6c65d6-21f8-0310-a445-fbbf500fbdde>
2007-11-03 01:13:38 +0000

    moving io classes to their own package

--- trunk/berkano-tools/berkano-util/src/test/java/net/incongru/io/FlusherTest.java
+++ trunk/berkano-tools/berkano-util/src/test/java/net/incongru/io/FlusherTest.java
@@ -1,11 +1,10 @@
-package net.incongru.util.io;
+package net.incongru.io;
 
 import junit.framework.TestCase;
 
 import java.io.Closeable;
+import java.io.Flushable;
 import java.io.IOException;
-import static org.easymock.EasyMock.*;
-
 
 /**
  *
@@ -13,24 +12,30 @@ import static org.easymock.EasyMock.*;
  * @author $Author: $ (last edit)
  * @version $Revision: $ 
  */
-public class CloserTest extends TestCase {
-    public void testProcessAndCloseAreOnlyCalledOnce() throws IOException {
-        DummyCloseable stream = new DummyCloseable();
-        new Closer<DummyCloseable>().processAndClose(stream, new IOProcessor<DummyCloseable>() {
-            public void process(DummyCloseable flux) throws IOException {
+public class FlusherTest extends TestCase {
+    public void testProcessFlushAndCloseAreOnlyCalledOnce() throws IOException {
+        FlushableCloseable stream = new FlushableCloseable();
+        new Flusher<FlushableCloseable>().processFlushAndClose(stream, new IOProcessor<FlushableCloseable>() {
+            public void process(FlushableCloseable flux) throws IOException {
                 flux.doStuff();
             }
         });
         stream.verify();
     }
 
-    private static final class DummyCloseable implements Closeable {
+    private static final class FlushableCloseable implements Flushable, Closeable {
         private boolean doCalled;
+        private boolean flushCalled;
         private boolean closeCalled;
 
         public void doStuff() {
             assertFalse("doStuff was called more than once", doCalled);
             doCalled = true;
+        }
+
+        public void flush() throws IOException {
+            assertFalse("flush was called more than once", flushCalled);
+            flushCalled = true;
         }
 
         public void close() throws IOException {
@@ -40,6 +45,7 @@ public class CloserTest extends TestCase {
 
         public void verify() {
             assertTrue("doStuff was not called", doCalled);
+            assertTrue("flush was not called", flushCalled);
             assertTrue("close was not called", closeCalled);
         }
     }
